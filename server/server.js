b58794a8c80d36eba00a96ecf32677827c792ebc
ItsssBobby <114122721+ItsssBobby@users.noreply.github.com>
2023-03-27 19:21:57 -0400

    server file redone by Robert/Cherry

--- server/server.js
+++ server/server.js
@@ -7,19 +7,24 @@ const db = require("./config/connection");
 const { authMiddleware } = require("./util/auth");
 const PORT = process.env.PORT || 3001;
 const app = express();
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: authMiddleware,
-});
+
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+//   context: authMiddleware,
+// });
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/"));
 });
+
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
