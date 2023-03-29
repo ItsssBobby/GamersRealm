598f8dd320dba0bd4ae7e94c758e31b68a29be7f
ItsssBobby <114122721+ItsssBobby@users.noreply.github.com>
2023-03-28 20:24:23 -0400

    added routes for current pages and changed app folder a lot

--- client/src/register.jsx
+++ client/src/register.jsx
@@ -1,29 +1,41 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
-export const Register = () =>{
-    const [email, setEmail] = useState('');
-    const [pass, setPass] = useState('');
-    const [name, setName] = useState('');
+export const Register = () => {
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
+  const [name, setName] = useState("");
 
-    const handleSubmit = (e) =>{
-        email.preventDefault();
-        console.log(email);
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email);
+  };
 
-
-    return (
-        <>
-            <form onSubmit={handleSubmit}>
-                <label>Full Name</label>
-                <input value={name} name="name" placeholder="full name"/>
-                <label htmlFor ="email">email</label>
-                <input value = {email} type="email"placeholder="youremail@email.com" id="email" name="email"/>
-                <label for ="password">password</label>
-                <input value ={pass} type="password"placeholder="*******" id="password" name="password"/>
-                <button>Log In</button>
-        </form>
-        <button onClick={()=> props.onFormSwitch('login')} >Already Have An Account Log In Here</button>
-    </>    
-       
-    )
-}
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <label>Full Name</label>
+        <input value={name} name="name" placeholder="full name" />
+        <label htmlFor="email">email</label>
+        <input
+          value={email}
+          type="email"
+          placeholder="youremail@email.com"
+          id="email"
+          name="email"
+        />
+        <label htmlFor="password">password</label>
+        <input
+          value={pass}
+          type="password"
+          placeholder="*******"
+          id="password"
+          name="password"
+        />
+        <button>Log In</button>
+      </form>
+      <button onClick={() => props.onFormSwitch("login")}>
+        Already Have An Account Log In Here
+      </button>
+    </>
+  );
+};
